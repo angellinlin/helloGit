4aa39558db961a13febd767c7a960bb9251739e6
admin <1196825077@qq.com>
2020-10-25 21:15:30 +0800

    添加文件

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * @author zhougl2
+ * @create 2020-10-25-21:01
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
